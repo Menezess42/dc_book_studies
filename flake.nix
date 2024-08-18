06be162fca10fe802ce01f47d86a0118417d1877
Menezess42 <arielmenezess@hotmail.com>
2024-08-18 19:33:00 -0300

    testando um outro formato de flake

--- flake.nix
+++ flake.nix
@@ -1,25 +1,57 @@
 {
-  description = "Ambiente Python com nix-flakes";
-
+  description = "This is my python dev: Data Science Libraries Edition";
+  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
   };
-
-  outputs = { self, nixpkgs }: {
-    devShells = {
-      default = nixpkgs.lib.mkShell {
-        buildInputs = [
-          nixpkgs.python310
-          nixpkgs.python310Packages.pip
-          nixpkgs.python310Packages.numpy
-          nixpkgs.python310Packages.pandas
-        ];
-
-        shellHook = ''
-          echo "Ambiente Python com nix-flakes ativado!"
-        '';
-      };
+  # Flake outputs
+  outputs = { self, nixpkgs, }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = let
+          # Use Python 3.11
+          python = pkgs.python312;
+        in pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = [
+            # Python plus helper tools
+            (python.withPackages (ps:
+              with ps; [
+                # notebook
+                jupyter
+                jupyterlab
+                ipykernel
+                ipython
+                # scientific computing
+                pandas # Data structures & tools
+                numpy # Array & matrices
+                scipy # Integral, solving differential, equations, optimizations)
+                # Visualization
+                matplotlib # plot & graphs
+                seaborn # heat maps, time series, violin plot
+                # Algorithmic Libraries
+                scikit-learn-extra # Machine learning: regression, classificatons,..
+                statsmodels # Ecplore data, estimate statistical models, & perform statistical test.
+                # Formatting
+                black
+                # Probabilistic
+                # pymc #support < 311
+              ]))
+          ];
+        };
+      });
     };
-  };
 }
-
