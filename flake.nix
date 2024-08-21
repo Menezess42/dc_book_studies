fc6de9467e9fd2c400d40c91bde5631959775a91
Menezess42 <arielmenezess@hotmail.com>
2024-08-21 16:56:43 -0300

    change from python12 to 11

--- flake.nix
+++ flake.nix
@@ -1,57 +1,29 @@
 {
-  description = "This is my python dev: Data Science Libraries Edition";
-  # Flake inputs
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
-  };
-  # Flake outputs
-  outputs = { self, nixpkgs, }:
-    let
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
-      # Helper to provide system-specific attributes
-      forAllSystems = f:
-        nixpkgs.lib.genAttrs allSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
-    in {
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = let
-          # Use Python 3.11
-          python = pkgs.python312;
-        in pkgs.mkShell {
-          # The Nix packages provided in the environment
-          packages = [
-            # Python plus helper tools
-            (python.withPackages (ps:
-              with ps; [
-                # notebook
-                jupyter
-                jupyterlab
-                ipykernel
-                ipython
-                # scientific computing
-                pandas # Data structures & tools
-                numpy # Array & matrices
-                scipy # Integral, solving differential, equations, optimizations)
-                # Visualization
-                matplotlib # plot & graphs
-                seaborn # heat maps, time series, violin plot
-                # Algorithmic Libraries
-                scikit-learn-extra # Machine learning: regression, classificatons,..
-                statsmodels # Ecplore data, estimate statistical models, & perform statistical test.
-                # Formatting
-                black
-                # Probabilistic
-                # pymc #support < 311
-              ]))
-          ];
-        };
-      });
-    };
+	description = "Python dev env";
+	inputs={
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+	outputs = {self, nixpkgs, ...}@inputs:
+	let
+		system = "x86_64-linux";
+	pkgs=nixpkgs.legacyPackages.${system};
+	in
+	{
+		devShells.${system}.default=
+			pkgs.mkShell
+			{
+				nativeBuildInputs = with pkgs; [
+					python311
+					python311Packages.tensorflow
+					python311Packages.numpy
+					python311Packages.pandas
+					python311Packages.matplotlib
+					python311Packages.scipy
+					python311Packages.scikit-learn
+					python311Packages.jupyterlab
+					python311Packages.pytorch
+				];
+			};
+	};
 }
+
