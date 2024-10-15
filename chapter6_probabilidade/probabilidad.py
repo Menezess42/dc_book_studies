b81a9d355ef3b31df9454dd3105bd879750d854a
Menezess42 <arielmenezess@hotmail.com>
2024-10-14 21:31:54 -0300

    started chapter7 hipotese e inferencia

--- chapter6_probabilidade/probabilidad.py
+++ chapter6_probabilidade/probabilidad.py
@@ -1,4 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import enum, random
+
 
 # um Enum é um conjuto tipado de valores enumerados que deixa
 # o código mais descritivo e legível.
@@ -6,12 +11,14 @@ class kid(enum.Enum):
     BOY = 0
     GIRL = 1
 
+
 def random_kid() -> kid:
     return random.choice([kid.BOY, kid.GIRL])
 
-both_girls=0
-older_girls=0
-either_girl=0
+
+both_girls = 0
+older_girls = 0
+either_girl = 0
 
 random.seed(0)
 
@@ -20,16 +27,17 @@ for _ in range(10000):
 
     older = random_kid()
     if older == kid.GIRL:
-        older_girls+=1
+        older_girls += 1
     if older == kid.GIRL and younger == kid.GIRL:
-        both_girls+=1
+        both_girls += 1
     if older == kid.GIRL or younger == kid.GIRL:
-        either_girl+=1
+        either_girl += 1
 
 
 # Função de densidade de probabilidade (PDF)
 def uniform_pdf(x: float) -> float:
     return 1 if 0 <= x < 1 else 0
+
 
 # Função de distribuição cumulativa (CDF)
 def uniform_cdf(x: float) -> float:
@@ -46,39 +54,44 @@ def uniform_cdf(x: float) -> float:
 # de sino e é totalmente determinada por dois parâmetros: u(mi)=>média, o(sigma)=>desvio padrão,
 # A média indica onde é o ponto central do sino e o desvio-padrão indica a sua largura.
 import math
-SQRT_TWO_PI = math.sqrt(2*math.pi)
 
-def normal_pdf(x: float, mu: float=0, sigma: float=1) -> float:
-    return (math.exp(-(x-mu)**2 / 2 / sigma ** 2) / (SQRT_TWO_PI*sigma))
+SQRT_TWO_PI = math.sqrt(2 * math.pi)
+
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return math.exp(-((x - mu) ** 2) / 2 / sigma**2) / (SQRT_TWO_PI * sigma)
+
 
 import matplotlib.pyplot as plt
+
+
 # Vamos plotar algumas figuras para conferir seu visual
 def normal_pdf_plot():
-    xs = [x/10 for x in range(-50,50)]
-    plt.plot(xs, [normal_pdf(x,sigma=1) for x in xs], '-', label='mu=0,sigma=1')
-    plt.plot(xs, [normal_pdf(x,sigma=2) for x in xs], '--', label='mu=0,sigma=1')
-    plt.plot(xs, [normal_pdf(x,sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
-    plt.plot(xs, [normal_pdf(x,mu=-1) for x in xs], ':', label='mu=-1,sigma=1')
+    xs = [x / 10 for x in range(-50, 50)]
+    plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], "-", label="mu=0,sigma=1")
+    plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], "--", label="mu=0,sigma=1")
+    plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ":", label="mu=0,sigma=0.5")
+    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], ":", label="mu=-1,sigma=1")
     plt.legend()
     plt.title("Various Normal pdfs")
-    plt.savefig('various_normal_pdfs.png')
+    plt.savefig("various_normal_pdfs.png")
     plt.show()
-
 
 
 # A CDF para distribuição normal não pode ser calculada de forma simples
 # mas podemos usar a função do erro que vem embutida na lib math
-def normal_cdf(x: float, mu: float=0, sigma: float=1) -> float:
-    return (1+math.erf((x-mu)/math.sqrt(2)/sigma)) / 2
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
 
 # Agora vamos plotar alguns normal_cdf
 def normal_cdf_plot():
-    xs = [x / 10.0 for x in range(-50,50)]
-    plt.plot(xs, [normal_cdf(x,sigma=1) for x in xs], '-', label="mu=0,sigma=1")
-    plt.plot(xs, [normal_cdf(x,sigma=2) for x in xs], '--', label="mu=0,sigma=2")
-    plt.plot(xs, [normal_cdf(x,sigma=0.5) for x in xs], ':', label="mu=0,sigma=0.5")
-    plt.plot(xs, [normal_cdf(x,mu=-1) for x in xs], '-', label="mu=-1,sigma=1")
-    plt.legend(loc=4) # no canto direito
+    xs = [x / 10.0 for x in range(-50, 50)]
+    plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], "-", label="mu=0,sigma=1")
+    plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], "--", label="mu=0,sigma=2")
+    plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ":", label="mu=0,sigma=0.5")
+    plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], "-", label="mu=-1,sigma=1")
+    plt.legend(loc=4)  # no canto direito
     plt.title("Various Normal CDFs")
     plt.savefig("./various_normal_cdfs.png")
     plt.show()
@@ -88,31 +101,36 @@ def normal_cdf_plot():
 # à probabilidade especificada. Não existe uma forma simples de computar essa inversão,
 # no entanto como a normal_cdf é contínua e está crescendo estritamente, podemos usar
 # busca binária.
-def inverse_normal_cdf(p: float, mu: float=0, sigma: float=1, tolerance: float=0.00001) -> float:
+def inverse_normal_cdf(
+    p: float, mu: float = 0, sigma: float = 1, tolerance: float = 0.00001
+) -> float:
     """Encontre o inverso aproximado usando a busca binária"""
     # se não for padrão, computa o padrão e redimensiona
-    if mu!=0 or sigma!=1:
-        return mu+sigma*inverse_normal_cdf(p, tolerance=tolerance)
-    low_z = -10.0 # normal_cdf(-10) é (muito próximo de) 0
-    hi_z = 10.0 # normal_cdf(10) é (muito próximo de) 1
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    low_z = -10.0  # normal_cdf(-10) é (muito próximo de) 0
+    hi_z = 10.0  # normal_cdf(10) é (muito próximo de) 1
     while hi_z - low_z > tolerance:
-        mid_z = (low_z + hi_z)/2 # considere o ponto médio
-        mid_p = normal_cdf(mid_z) # e o valor de CDF
+        mid_z = (low_z + hi_z) / 2  # considere o ponto médio
+        mid_p = normal_cdf(mid_z)  # e o valor de CDF
         if mid_p < p:
-            low_z = mid_z # O ponto médio é muito baixo, procure um maior
+            low_z = mid_z  # O ponto médio é muito baixo, procure um maior
         else:
-            hi_z = mid_z # o ponto médio é muito alto, procure um menor
+            hi_z = mid_z  # o ponto médio é muito alto, procure um menor
 
     return mid_z
+
 
 # Teorema do Limite Central
 def bernoulli_trial(p: float) -> int:
     """Retorna 1 com probabilidade p e 0 com probabilidade 1-p"""
     return 1 if random.random() < p else 0
 
+
 def binomial(n: int, p: float) -> int:
     """Retorna a soma de n trial bernoulli(p)"""
     return sum(bernoulli_trial(p) for _ in range(n))
+
 
 # A média de uma variável bernoulli(p) é p e seu desvio-padrão,
 # raiz(p(1-p)). Segundo o teorema do limite central, à medida
@@ -121,23 +139,27 @@ def binomial(n: int, p: float) -> int:
 # o=raiz(np(1-p)).
 # Plotamos os dois para observar claramente a semelhança.
 from collections import Counter
+
+
 def binomial_histogram(p: float, n: int, num_points: int) -> None:
     """Seleciona pontos de um Binomial(n,p) e plota seu histograma"""
-    data = [binomial(n,p) for _ in range(num_points)]
+    data = [binomial(n, p) for _ in range(num_points)]
 
     # Use um gráfico de barras para indicar as amostras de binomiais
     histogram = Counter(data)
-    plt.bar([x - 0.4 for x in histogram.keys()],
-            [v / num_points for v in histogram.values()],
-            0.8,
-            color='0.75')
+    plt.bar(
+        [x - 0.4 for x in histogram.keys()],
+        [v / num_points for v in histogram.values()],
+        0.8,
+        color="0.75",
+    )
 
-    mu = p*n
-    sigma = math.sqrt(n*p*(1-p))
+    mu = p * n
+    sigma = math.sqrt(n * p * (1 - p))
 
     # Use um gŕafico de linhas para indicar a aproximação normal
-    xs = range(min(data), max(data)+1)
-    ys = [normal_cdf(i+0.5,mu,sigma) - normal_cdf(i-0.5, mu, sigma) for i in xs]
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma) for i in xs]
     plt.plot(xs, ys)
     plt.title("Binomial Distribution vs. Normal Aproximation")
     plt.show()
@@ -145,23 +167,8 @@ def binomial_histogram(p: float, n: int, num_points: int) -> None:
 
 if __name__ == "__main__":
     normal_cdf_plot()
-    print(f"(Both | older): {both_girls / older_girls}") # 0.514 - 1/2
-    print(f"(Both | either): {both_girls / either_girl}") # 0.342 - 1/3
+    print(f"(Both | older): {both_girls / older_girls}")  # 0.514 - 1/2
+    print(f"(Both | either): {both_girls / either_girl}")  # 0.342 - 1/3
     normal_pdf_plot()
     print(inverse_normal_cdf(0.9))
     binomial_histogram(0.75, 100, 10000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ chapter7_hipotese_inferencia/hip_inf.py
@@ -0,0 +1,179 @@
+# Teste estatístico de Hipótese
+# Muitas vezes DCientits fazem teste para confirmar
+# se uma hipótese é verdadeira. Tipo afirmações como
+# "é mais provável que as pessoas saim da página sem ler
+# o conteúdo quando surge um anúncio em uma janela pop-up
+# com um botão de fechar pequeno e inacaessível".
+
+# O esquema clássico contém a hipótese nula h0, que representa
+# uam posição padrão, e uma hipótese h1 para comparação.
+# Aplicamos estatística para decidir se h0 é falsa ou não.
+
+# EXEMPLO LANÇAMENTO DA MOEDA
+# Queremos testar a honestidade de uma moeda. Partimos da premissa
+# de que a moeda tem a probabilidade p de dar cara; então, a hipótese
+# nula é de que a moeda seja honesta -- ou seja, de que p=0.5. Testaremos
+# essa premissa em comparação com hipótese alternativa p!=0.5.
+
+# Especificamente, o teste consistirá em n lançamentos da moeda e na contagem do
+# número de X de caras. Cada lançamento da moeda é um ensaio de Bernouli, ou seja,
+# X é variável aleatória Binomial(n,p), que podemos aproximar usando a distribuição
+# normal
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from typing import Tuple
+import math
+
+
+def normal_approximation_to_binomial(n: int, p: float) -> Tuple[float, float]:
+    """Retorna mu e sigma correspondentes Binomial(n, p)"""
+    mu = p * n
+    sigma = math.sqrt(p * (1 - p) * n)
+    return mu, sigma
+
+
+# Sempre que uma variável aleatória segue uma distribuição normal, é possível
+# aplicar o normal_cdf para descobrir a probabilidade de o seu valor realizado estar
+# contido (ou não) em um determinado intervalo
+from chapter6_probabilidade.probabilidad import normal_cdf
+
+# o normal cdf é a probabilidade de a variável estar abaixo de um limite
+normal_probability_below = normal_cdf
+
+
+# Está acima do limite se não está abaixo do limite
+def normal_probability_above(lo: float, mu: float = 0, sigma: float = 1) -> float:
+    """A probabilidade de que N(mu, sigma) seja maior do que lo."""
+    return 1 - normal_cdf(lo, mu, sigma)
+
+
+# Está entre se é menor que hi, mas não menor do que lo
+def normal_probability_between(
+    lo: float, hi: float, mu: float = 0, sigma: float = 1
+) -> float:
+    """A probabilidade de que um N(mu, sigma) esteja entre lo e hi"""
+    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+
+# Está fora se não está dentro
+def normal_probability_outside(
+    lo: float, hi: float, mu: float = 0, sigma: float = 1
+) -> float:
+    """A probabilidade de que um N(mu, sigma) não esteja entre lo e hi"""
+    return 1 - normal_probability_between(lo, hi, mu, sigma)
+
+
+# Também podemos fazer o contrário e encontrar a região fora do limiar ou
+# o intervalo (simétrico) em torno da média que está associado a um determinado
+# nível de probabilidade. Por exemplo, para encontrar o intervalo centrado
+# na média que contém 60% da probabilidade, é preciso determinar os limiares
+# inferirores e superiores que contêm, individualmente 20% da probabilidade (deixando 60%)
+
+from chapter6_probabilidade.probabilidad import inverse_normal_cdf
+
+
+def normal_upper_bound(probability: float, mu: float = 0, sigma: float = 1) -> float:
+    """Retorna o z para o qual P(z <= Z) = probabilidade"""
+    return inverse_normal_cdf(probability, mu, sigma)
+
+
+def normal_lower_bound(probability: float, mu: float = 0, sigma: float = 1) -> float:
+    """Retorna o z pra o qual P(z>=1) = probabilidade"""
+    return inverse_normal_cdf(1 - probability, mu, sigma)
+
+
+def normal_two_sided_bounds(
+    probability: float, mu: float = 0, sigma: float = 1
+) -> Tuple[float, float]:
+    """retorna os limites simétricos (relativos à média) que contêm a probabilidade especificada"""
+    tail_probability = (1 - probability) / 2
+
+    # o limite superior deve estar abaixo de tail_probability
+    upper_bound = normal_lower_bound(tail_probability, mu, sigma)
+
+    # O limite inferior deve estar acima de tail_probability
+    lower_bound = normal_upper_bound(tail_probability, mu, sigma)
+
+    return lower_bound, upper_bound
+
+
+if __name__ == "__main__":
+    # Digamos que a moeda será lançada n=mil vezes.
+    # Se a hipótese de honestidade estiver correta,
+    # X estará distribuido, aproximadamente, de modo
+    # normal com média de 500 e desvio-padrão de 15.8
+    mu_0, sigma_0 = normal_approximation_to_binomial(1000, 0.5)
+    print(f"mu_0:{mu_0}\nsigma_0:{sigma_0}")
+
+    # Precisamos definir a significancia -- Determinar nosso nível
+    # de disposição para obter um erro tipo 1 ("falso positivo")
+    # e recusar a H0 mesmo se ela for verdadeira.
+    # essa disposição costuma ser definida em 5% ou 1%. Aqui, escolhemos
+    # 5%
+    # O teste recusará H0 se X estiver fora dos limites obtidos da seguinte
+    # forma:
+    lower_bound, upper_bound = normal_two_sided_bounds(0.95, mu_0, sigma_0)
+    print(f"Lower Bound:{lower_bound}\nUpper Bound:{upper_bound}")
+    # Partindo da premissa de que p é igual a 0.5(h0 verdadeiro), há uma
+    # probabilidade de apenas 5% de observarmos um X fora desse intervalo,
+    # exatamente a significancia determinada. Em outras palavras, se h0 é verdadeira,
+    # então o teste indica o resultado correto em, aproximadamente, 19 de 20 vezes.
+
+    # Também queremos determinar a potência do teste, a probabilidade de não
+    # ocorrer um erro tipo 2, que acontece quando falhamos na recusa de uma h0 falsa.
+    # Para medir isso, temos que especificar o significado de uma h0 falsa.
+    # (saber que p não é 0.5 não fornce muita informação sobre a distribuição de X).
+    # Especificamente, queremos verificar o que acontece se p realmente for 0.55;
+    # nesse caso, a moeda estará levemente viciada em cara.
+    # Aqui podemos calcular a potência do teste da seguinte forma:
+
+    # limites de 95% baseados na premissa de que p=0.5
+    lo, hi = normal_two_sided_bounds(0.95, mu_0, sigma_0)
+    print(f"limites de 95% baseados na premissa de que p=0.5\n\tlo:{lo}\n\thi:{hi}")
+
+    # mu e sigma reais baseados em p=0.55
+    mu_1, sigma_1 = normal_approximation_to_binomial(1000,0.55)
+    print(f"mu e sigma reais baseados em p=0.55\n\tmu_1:{mu_1}\n\tsigma_1:{sigma_1}")
+
+    # Um erro tipo 2 ocorre quando falhamos em rejeitar a hipótese nula, o que ocorre
+    # quando X ainda está no intervalo original
+    type_2_probability = normal_probability_between(lo, hi, mu_1, sigma_1)
+    print(f" Um erro tipo 2 ocorre quando falhamos em rejeitar a hipótese nula, o que ocorre quando X ainda está no intervalo original\n\tType_2_probability:{type_2_probability} ")
+    power = 1 - type_2_probability # 0.887
+    print(f"Power of type_2 in %:\n\tPower:{power}")
+
+    # Agora imagine que a hipótese nula indica que a moeda não está viciada em cara,
+    # ou seja, que p<=0.5. Nesse caso, queremos um teste unilateral pra rejeitar a
+    # hipótese nula quando X é muito maior que 50, mas não quando X é menor. Portanto,
+    # o teste de significancia de 5% deve aplicar o normal_probability_below para
+    # encontrar o limiar que fica sobre probabilidade de 95%:
+
+    hi = normal_upper_bound(0.95, mu_0, sigma_0)
+    # é 526 (<531, já que precisamos de mais probabilidade no ponto superior)
+    type_2_probability = normal_probability_below(hi, mu_1, sigma_1)
+    power = 1 - type_2_probability # 0.936
+    print(f"Power of type_2 in %:\n\tPower:{power}")
+
+    # Esse teste tem uma potência maior, pois não recusa mais a h0 quando X é
+    # menor do que 469 (algo muito improvável de acontecer se h1 for verdadeiro);
+    # em vez disso, ele recusa a h0 quando X está entre 526 e 531 (algo provável
+    # de acontecer se h1 for verdadeiro).
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
